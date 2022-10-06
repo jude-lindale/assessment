8d3c7b9d2005dc12d58f3d2420ccfdfca2b26370
jude-lindale <70959569+jude-lindale@users.noreply.github.com>
2022-10-06 10:20:11 -0400

    Update server.sh

--- server.sh
+++ server.sh
@@ -2,6 +2,6 @@ yum install -y httpd
 systemctl enable httpd
 systemctl start httpd
 systemctl status httpd
-firewall-cmd --permanent --ad-port=80/tcp
+firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --reload
 firewall-cmd --query-port=80/tcp
