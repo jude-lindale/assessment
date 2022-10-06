e3c2200bcc057dd06b275a793fdf8480d239aa1a
jude-lindale <70959569+jude-lindale@users.noreply.github.com>
2022-10-06 10:32:19 -0400

    Update server.sh

--- server.sh
+++ server.sh
@@ -5,3 +5,4 @@ systemctl status httpd
 firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --reload
 firewall-cmd --query-port=80/tcp
+openssl req -newkey rsa:2048 -keyout websrv.key -out websrv.csr
