9f216c72c8688c386cd222ffb24c086fe083d063
jude-lindale <70959569+jude-lindale@users.noreply.github.com>
2022-10-06 10:38:41 -0400

    Update ca.sh

--- ca.sh
+++ ca.sh
@@ -1,11 +1,11 @@
 useradd jude
 passwd jude
 firewall-cmd --permanent --add-port=22/tcp
-firewall-cmd --permanent --add-port=22/scdp
+firewall-cmd --permanent --add-port=22/sctp
 firewall-cmd --reload
 cd /etc/pki/CA
 touch index.txt
 echo 1000 > serial
 openssl genrsa -des3 -out private/cakey.pem 2048
 openssl req -new -x509 -days 365 -key private/cakey.pem -out cacert.pem
-openssl ca -out websrv.crt -infiles websrv.csr
+
